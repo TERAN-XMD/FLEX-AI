5f5f27562fc740aaa7aa57ff9fdf012d895b8bbe
TERAN-XMD <earnonline6k@gmail.com>
2025-08-12 13:41:11 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "FLEX-AI~",
+SESSION_ID: process.env.SESSION_ID || "FLEX-AI~xMA1FBJL#fSx1Vueqc4Z1xdgowIuIRDuTfGsZJwmT-stOdXkSr-U",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
